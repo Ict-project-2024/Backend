5cad0d63659645fdea853f7c75e047920aa1247c
Nuwan Subhasingha <126569796+NuwanSubasingha99@users.noreply.github.com>
2024-07-13 23:27:30 +0530

    add middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import roleRoute from './routes/role.route.js';
+import authRoute from './routes/auth.route.js';
 
 const app = express();
 
@@ -9,6 +10,20 @@ dotenv.config();
 app.use(express.json());
 
 app.use("/api/role", roleRoute);
+app.use("/api/auth", authRoute)
+
+
+app.use((obj, req, res, next) => {
+  const statusCode = obj.status || 500;
+  const message = obj.message || "something went wrong..";
+  return res.status(statusCode).json({
+    success: [200, 201, 204].some(a => a === obj.status) ? true : false,
+    status: statusCode,
+    message: message,
+    data:obj.data,
+    stack: obj.stack
+  })
+})
 
 mongoose
   .connect(
