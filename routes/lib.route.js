bc29b033a6f94b52871989daa9e27f9e4526963e
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-16 20:00:18 +0530

    refactor: Add viewUserAccess route to retrieve user access history for library and medical center

--- routes/lib.route.js
+++ routes/lib.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { enterLibrary, exitLibrary, viewTrafficStatus, viewHistory } from '../controllers/Lib.controller.js';
+import { enterLibrary, exitLibrary, viewTrafficStatus, viewHistory, viewUserAccess } from '../controllers/Lib.controller.js';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/enter', enterLibrary);
 router.post('/exit', exitLibrary);
 router.post('/status', viewTrafficStatus);
 router.get('/history', viewHistory);
+router.post('/useraccess', viewUserAccess);
 
 export default router;
